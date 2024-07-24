d2f7cddff72f3491dc6059a2783124b93c4a19dc
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-07-24 18:25:23 +0300

    some files

---
+++ script.js
@@ -0,0 +1,52 @@
+console.log("Poggers !");
+
+var database = {
+    "collections": {
+        "animals": {
+            "description": "This is a animal collection",
+            "appearance": "yellow",
+            "words" : [
+                { "frog": "カエル" },
+                { "lion": "ライオン" },
+                { "panda": "パンダ"},
+                { "octopus": "タコ"}
+            ]
+        },
+        "numbers": {
+            "description": "This is a numbers collection",
+            "appearance": "blue",
+            "words" : [
+                { "one": "いち" },
+                { "two": "に" },
+                { "three": "さん" },
+                { "four": "し" },
+                { "five": "ご" },
+                { "six": "ろく" },
+                { "seven": "しち" },
+                { "eight": "はち" },
+                { "nine": "きゅう" },
+                { "ten": "じゅう" },
+                { "eleven": "じゅういち" },
+                { "twelve": "じゅうに" },
+                { "twenty": "にじゅう" },
+                { "twenty one": "にじゅうに" },
+                { "thirty": "さんじゅう" },
+                { "forty": "しじゅう" },
+                { "one hundred": "ひゃく" }
+            ]
+        }
+    }
+};
+
+var form = document.getElementById("form-id");
+
+var text_submit = document.getElementById("text_submit");
+var submit_button = document.getElementById("submit_button");
+
+submit_button.addEventListener("click", function (event) {
+    const obj = JSON.parse(text_submit.value);
+
+    console.log(obj);
+
+    event.preventDefault();
+});
