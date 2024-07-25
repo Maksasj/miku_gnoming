91bf9efd4955b18e57d12072a2df4b3530e08fcd
maksasj <radioboos@gmail.com>
2024-07-25 18:02:32 +0300

    new design

--- script.js
+++ script.js
@@ -20,23 +20,74 @@ var active_database = {
             "description": "This is a numbers collection",
             "appearance": "blue",
             "words" : [
-                { "one": "いち" },
-                { "two": "に" },
-                { "three": "さん" },
-                { "four": "し" },
-                { "five": "ご" },
-                { "six": "ろく" },
-                { "seven": "しち" },
-                { "eight": "はち" },
-                { "nine": "きゅう" },
-                { "ten": "じゅう" },
-                { "eleven": "じゅういち" },
-                { "twelve": "じゅうに" },
-                { "twenty": "にじゅう" },
-                { "twenty one": "にじゅうに" },
-                { "thirty": "さんじゅう" },
-                { "forty": "しじゅう" },
-                { "one hundred": "ひゃく" }
+                { 
+                    "word": "one",
+                    "variants": [ "いち" ] 
+                },
+                { 
+                    "word": "two",
+                    "variants": [ "に" ] 
+                },
+                { 
+                    "word": "three",
+                    "variants": [ "さん" ] 
+                },
+                { 
+                    "word": "four",
+                    "variants": [ "し" ] 
+                },
+                { 
+                    "word": "five",
+                    "variants": [ "ご" ] 
+                },
+                { 
+                    "word": "six",
+                    "variants": [ "ろく" ] 
+                },
+                { 
+                    "word": "seven",
+                    "variants": [ "しち" ] 
+                },
+                { 
+                    "word": "eight",
+                    "variants": [ "はち" ] 
+                },
+                {   
+                    "word": "nine",
+                    "variants": [ "きゅう" ] 
+                },
+                { 
+                    "word": "ten",
+                    "variants": [ "じゅう" ] 
+                },
+                {   
+                    "word": "eleven",
+                    "variants": [ "じゅういち" ] 
+                },
+                { 
+                    "word": "twelve",
+                    "variants": [ "じゅうに" ] 
+                },
+                { 
+                    "word": "twenty",
+                    "variants": [ "にじゅう" ] 
+                },
+                { 
+                    "word": "twenty one",
+                    "variants": [ "にじゅうに" ]
+                },
+                { 
+                    "word": "thirty",
+                    "variants": [ "さんじゅう" ] 
+                },
+                { 
+                    "word": "forty",
+                    "variants": [ "しじゅう" ] 
+                },
+                { 
+                    "word": "one hundred",
+                    "variants": [ "ひゃく" ] 
+                }
             ]
         }
     }
@@ -53,4 +104,40 @@ submit_button.addEventListener("click", function (event) {
     console.log(obj);
 
     event.preventDefault();
+});
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+var next_button = document.getElementById("next_button");
+
+var card_question = document.getElementById("card_question");
+var card_result = document.getElementById("card_result");
+
+next_button.addEventListener("click", function (event) {
+    let words = active_database["collections"]["numbers"]["words"];
+    var random_word = words[getRandomInt(words.length)]
+
+    card_question.innerHTML = "What word matches '" + random_word["word"] + "' ?";
+
+    let variants = []
+
+    variants.push(random_word["variants"][0]);
+
+    for(let i = 0; i < 3; ++i) {
+        var thing = words[getRandomInt(words.length)]["variants"][0];
+        variants.push(thing);
+    }
+
+    card_result.innerHTML = "";
+
+    shuffleArray(variants);
+
+    for(let i = 0; i < 4; ++i) {
+        card_result.innerHTML += "<hr id='card_separator'></hr><button class='click'>" + variants[i] + "</button>" + ""
+    }
 });
